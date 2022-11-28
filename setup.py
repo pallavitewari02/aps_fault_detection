26a884dc768ade1865b227515cf2f50965ddaa14
pallavitewari02 <pallavics2004@gmail.com>
2022-11-28 17:30:53 +0530

    updated the requirements.txt

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages,setup
+
+
+def get_requirements():
+    pass
+
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="pallavi",
+    author_email="pallavics2004@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+)
